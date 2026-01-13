6cc7c7c0172f0205c3a86e3fc8e7fc35830e1ee5
Alagbaoso Solomon Ginikachukwu <solomonginika85@gmail.com>
2026-01-13 01:30:12 +0100

    code eddited

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -5,5 +5,13 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Sandbox Project.");
+
+       int number = 5;
+       number = 8;
+       number = number + 3;
+       string colour = "blue";
+
+
+
     }
 }
