a6fe4bd6f185817220f3b54b84c4df17514fb262
Alagbaoso Solomon Ginikachukwu <solomonginika85@gmail.com>
2026-01-13 02:07:42 +0100

    changes made

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -1,9 +1,69 @@
 using System;
 
 class Program
+
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("What is your grade percentage: ");
+        string answer = Console.ReadLine();
+        int percent = int.Parse(answer);
+
+        string letter = "";
+        if (percent >= 90)
+        {
+            letter = "A";
+        }
+        else if (percent >= 80)
+        {
+            letter = "B";
+        }
+        else if (percent >= 70)
+        {
+            letter = "C";
+        }
+        else if (percent >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        string sign = "";
+        if (letter != "F")
+        {
+            int lastDigit = percent % 10;
+            if (lastDigit >= 7)
+            {
+                sign = "+";
+            }
+            else if (lastDigit < 3)
+            {
+                sign = "-";
+            }
+        }
+
+        // Handle exceptional cases
+        if (letter == "A" && sign == "+")
+        {
+            sign = ""; // No A+
+        }
+        if (letter == "F")
+        {
+            sign = ""; // No F+ or F-
+        }
+
+        Console.WriteLine($"Your letter grade is: {letter}{sign}");
+
+        if (percent >= 70)
+        {
+            Console.WriteLine("Congratulations, you passed the course!");
+        }
+        else
+        {
+            Console.WriteLine("Don't worry, try again next time!");
+        }
     }
 }
