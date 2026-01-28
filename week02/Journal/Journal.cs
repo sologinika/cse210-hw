1680ff3ab1a949d28ded4a27b1a71014877bb412
Alagbaoso Solomon Ginikachukwu <solomonginika85@gmail.com>
2026-01-29 00:19:42 +0100

    added files: week02/Journal/Entry.cs, week02/Journal/Journal.cs, week02/Journal/PromptGenerator.cs

---
+++ week02/Journal/Journal.cs
@@ -0,0 +1,20 @@
+public class Journal
+{
+    public List<Entry> _entries;
+
+    public void AddEntry(Entry newentries)
+
+    {
+    }
+public void DisplayAll()
+    {
+    }
+
+    public void SaveToFile(string filename)
+    {
+    }
+
+    public void LoadFromFile(string filename)
+    {
+    }
+}
---
+++ week02/Journal/PromptGenerator.cs
@@ -0,0 +1,11 @@
+public class PromptGenerator
+{
+    public List<string> _prompts;
+
+    public string GetRandomPrompt()
+
+    {
+    return "";
+    }
+
+}
